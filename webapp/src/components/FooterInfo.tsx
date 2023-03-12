d80c0190dc2e6ae7271f4004b0c29aeedc7b9411
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-03-12 18:47:57 +0100

    12/03/2023 - Omar - Creación del footer de la aplicación

--- webapp/src/components/FooterInfo.tsx
+++ webapp/src/components/FooterInfo.tsx
@@ -2,7 +2,7 @@ import "../css/footer.css";
 function FooterInfo() {
     return (
         <footer id="info">
-            <p><a id="link" href="https://arquisoft.github.io/">© Arquisoft - Uniovi</a></p>
+            <p><a id="link" href="https://arquisoft.github.io/">© Arquisoft - UNIOVI</a></p>
         </footer>
     )
 }
