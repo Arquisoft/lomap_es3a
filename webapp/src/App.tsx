5511ce4c988ec607cc814ef1cb3a6e2c82c4f781
Raúl España <uo278036@uniovi.es>
2023-04-03 13:58:44 +0200

    03/04/2023 - Raúl - Creado Switch para DarkMode.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,16 +1,42 @@
-import {SessionProvider} from "@inrupt/solid-ui-react";
-import React from 'react';
-import './App.css';
+import { SessionProvider } from "@inrupt/solid-ui-react";
+import React, { useState } from "react";
+import "./App.css";
 import ViewPaths from "./components/ViewPaths";
+import Switch, { SwitchProps } from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
 
+import { useTheme } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from "@mui/material";
+import {blue, pink} from "@mui/material/colors";
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: blue[500],
+        },
+        secondary: {
+            main: pink[500],
+        },
+    },
+});
 function App(): JSX.Element {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const theme = useTheme();
+
+    const handleSwitchChange = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+
+
 
     return (
-        <SessionProvider session-id="login">
-            <div>
+
+            <div className="App">
                 <ViewPaths/>
             </div>
-        </SessionProvider>
     );
 }
 
