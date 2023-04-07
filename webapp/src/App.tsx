094e497002b77a7f5b5c68066f740cab58be738f
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-04-07 03:21:22 +0200

    07/04/2023 - Omar - Eliminado Dark Mode.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,42 +1,16 @@
-import { SessionProvider } from "@inrupt/solid-ui-react";
-import React, { useState } from "react";
-import "./App.css";
+import {SessionProvider} from "@inrupt/solid-ui-react";
+import React from 'react';
+import './App.css';
 import ViewPaths from "./components/ViewPaths";
-import Switch, { SwitchProps } from '@mui/material/Switch';
-import { styled } from '@mui/material/styles';
 
-import { useTheme } from '@mui/material/styles';
-import {createTheme, ThemeProvider} from "@mui/material";
-import {blue, pink} from "@mui/material/colors";
-
-const theme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: blue[500],
-        },
-        secondary: {
-            main: pink[500],
-        },
-    },
-});
 function App(): JSX.Element {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-
-    const theme = useTheme();
-
-    const handleSwitchChange = () => {
-        setIsDarkMode(!isDarkMode);
-    };
-
-
-
 
     return (
-
-            <div className="App">
+        <SessionProvider session-id="login">
+            <div>
                 <ViewPaths/>
             </div>
+        </SessionProvider>
     );
 }
 
