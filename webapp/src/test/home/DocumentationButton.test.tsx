d4ed95b4f24de35d9b051de05fbc4bf8730e3cdd
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-04-09 15:01:37 +0200

    09/04/2023 - Omar - Realizado test de la pila de tecnologías

--- webapp/src/test/home/DocumentationButton.test.tsx
+++ webapp/src/test/home/DocumentationButton.test.tsx
@@ -1,14 +1,7 @@
-import { render } from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
 import DocumentationButton from "../../components/home/DocumentationButton";
 
 test("Documentation button renders correctly", async () => {
-    const {getByText} = render(<DocumentationButton href={"https://arquisoft.github.io/lomap_es3a/"}
-                                                    text={"See our documentation"}/>);
-    expect(getByText("See our documentation")).toBeInTheDocument();
-});
-
-test("Documentation button redirects to documentation", async () => {
-    const {getByText} = render(<DocumentationButton href={"https://arquisoft.github.io/lomap_es3a/"}
-                                                    text={"See our documentation"}/>);
-    expect(getByText("See our documentation")).toBeInTheDocument();
+    const {getByText} = render(<DocumentationButton/>);
+    expect(getByText("docs_button")).toBeInTheDocument();
 });
