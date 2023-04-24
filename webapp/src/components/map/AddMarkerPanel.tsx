c9f792fcb9fe07f7f13e902d170c2915221e75d3
Omitg24 <omartg24@gmail.com>
2023-04-24 10:27:26 +0200

    24/04/2023 - Omar - Finalizada creación de mapas

--- webapp/src/components/map/AddMarkerPanel.tsx
+++ webapp/src/components/map/AddMarkerPanel.tsx
@@ -5,7 +5,6 @@ import Comments from "./options/Comments";
 import Coordinates from "./options/Coordinates";
 import React from "react";
 import {initReactI18next, useTranslation} from "react-i18next";
-import Mark from "./options/Mark";
 import i18n from "../../i18n";
 
 i18n.use(initReactI18next)
