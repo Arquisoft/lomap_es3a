71c16287390fc97bad43e814d4eb81fa5dcf6ffd
uo284373 <uo284373@uniovi.es>
2023-04-26 14:01:04 +0200

    26/04/2023 - Carlos - Mejora en el código de la función que busca los amigos

--- webapp/src/components/pod/FriendsPOD.ts
+++ webapp/src/components/pod/FriendsPOD.ts
@@ -32,40 +32,21 @@ export interface PersonData {
     friends: string[]
 }
 
-const findFullPersonProfile = async (
-    webId: IriString,
-    session: Session,
-    visited = new Set<IriString>(),
-    response: SolidDataset[] = [],
-    fail = true,
-    iri = webId,
-): Promise<SolidDataset[]> => {
+async function findFullPersonProfile(webId: string, session: Session, response: SolidDataset[] = []){
     try {
-        visited.add(iri)
-        const dataset = await getSolidDataset(iri, {fetch: session.fetch})
+        const dataset = await getSolidDataset(webId, {fetch: session.fetch})
         const person = getThing(dataset, webId)
         if (person) {
             response.push(dataset)
-            const same: string[] = getTermAll(person, owl.sameAs).map(a => a.value)
-            const see: string[] = getTermAll(person, rdfs.seeAlso).map(a => a.value)
-
-            for (const uri of [...same, ...see]) {
-                console.log('extending', uri)
-                if (!visited.has(uri))
-                    await findFullPersonProfile(webId, session, visited, response, false, uri)
-            }
         }
     } catch (e) {
-        if (fail) throw e
+        throw e
     }
     return response
 }
 
 
-
-
-
-export const findPersonData = async (session: Session,webId: IriString): Promise<PersonData> => {
+export async function findPersonData(session: Session,webId: IriString){
     const data: PersonData = { webId: webId, photo:'',name: '', friends: [] }
     if (webId) {
         const dataset = await findFullPersonProfile(webId, session)
