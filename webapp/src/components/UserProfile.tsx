50f43e71f15ac06fc83e76cd713b1339d8a4a759
Raúl España <uo278036@uniovi.es>
2023-04-27 15:23:27 +0200

    27/04/2023 - Raúl - Creada lista amigos perfil

--- webapp/src/components/UserProfile.tsx
+++ webapp/src/components/UserProfile.tsx
@@ -1,64 +1,146 @@
-import React from 'react';
-import {Avatar, Box, Button, Typography} from '@mui/material';
-import {CombinedDataProvider, Image, Text, useSession} from "@inrupt/solid-ui-react";
-import {FOAF, VCARD} from "@inrupt/lit-generated-vocab-common";
+import {CombinedDataProvider, Image, useSession} from "@inrupt/solid-ui-react";
+import React, {useEffect, useState} from "react";
+import {addFriendToPod, changePermissions, findPersonData, PersonData, removeFriendFromPOD} from "./pod/FriendsPOD";
+import {useTranslation} from "react-i18next";
+import "../css/friends.css"
+import Avatar from "@mui/material/Avatar";
+import {VCARD} from "@inrupt/lit-generated-vocab-common";
+import Icon from "../img/symbols/GOMapSymbol.png";
+import Notification from "./Notification";
+import profilePhoto from "../img/profile.png";
 
 
-interface UserProfileProps {
 
+function ManageFriends(){
+    const { session } = useSession();
+    const [idp, setIdp] = useState("https://inrupt.net");
+    const [error,setError] = useState(false)
+    const [friendAdd, setFriendAdd] = useState(false)
+    const [friendRemove, setFriendRemove] = useState(false)
+    const [friendPermissions,setFriendPermissions] = useState(false)
+    const [personData, setPersonData] = useState<PersonData>({ webId: '',photo: '', name: '', friends: [] })
+    const {webId} = session.info;
+    const [friends, setFriendList] = useState<PersonData[]>([]);
+    const [showButtonAdd,setShowButtonAdd] = useState(true)
+    const { t } = useTranslation();
+
+    const providers = [
+        { name: "Inrupt", value: "https://inrupt.net" },
+        { name: "SolidCommunity", value: "https://solidcommunity.net" },
+    ];
+
+
+    useEffect(() => {
+        async function loadPersonData() {
+            if (webId !== undefined) {
+                const data = await findPersonData(session,webId)
+                if(data){
+                    setPersonData(data)
+                }
+            }
+        }
+
+        loadPersonData()
+    }, [webId,session])
+
+    useEffect(() => {
+        async function fetchFriends() {
+            if (personData.friends.length > 0) {
+                const names = await Promise.all(
+                    personData.friends.map((friend) => findPersonData(session,friend))
+                );
+                setFriendList(names);
+            }
+        }
+        fetchFriends()
+    }, [personData.friends, session])
+
+    function handleCloseNotification(){
+        setError(false)
+        setFriendAdd(false)
+        setFriendRemove(false)
+        setFriendPermissions(false)
+    }
+
+
+    return(
+        <div id="friendsConfiguration">
+            <div id="friendsConfigurationBody">
+                <h1>{t("friends")}</h1>
+                {friends.length > 0 ? (
+                    <div id="friendsTable">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>{t("friendName")}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {friends.map((friend) => (
+                                <tr key={friend.webId}>
+                                    <td>
+                                        <div id="friendNamePhoto">
+                                            <CombinedDataProvider datasetUrl={friend.webId} thingUrl={friend.webId}>
+                                                <div className="friendAvatar">
+                                                    {friend.photo !== "" ? (
+                                                        <Image property={VCARD.hasPhoto.iri.value} width={65}/>
+                                                    ) : (
+                                                        <img src={profilePhoto} width={65} alt={friend.name}/>
+                                                    )}
+                                                </div>
+                                            </CombinedDataProvider>
+                                            <div className="friendName">{friend.name}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                ) : (
+                    <div className="no-content" id="noFriendsProfile">
+                        <p>{t("notificationNoFriends")}</p>
+                    </div>
+                )}
+            </div>
+
+            {error && (
+                <Notification
+                    title={t("notificationAddFriendErrorTitle")}
+                    message={t("notificationAddFriendError")}
+                    time={t("notificationTime")}
+                    icon={Icon}
+                    onClose={handleCloseNotification}
+                />
+            )}
+            {friendAdd && (
+                <Notification
+                    title={t("notificationAddFriendTitle")}
+                    message={t("notificationAddFriend")}
+                    time={t("notificationTime")}
+                    icon={Icon}
+                    onClose={handleCloseNotification}
+                />
+            )}
+            {friendRemove && (
+                <Notification
+                    title={t("notificationRemoveFriendTitle")}
+                    message={t("notificationRemoveFriend")}
+                    time={t("notificationTime")}
+                    icon={Icon}
+                    onClose={handleCloseNotification}
+                />
+            )}
+            {friendPermissions && (
+                <Notification
+                    title={t("notificationPermissionsFriendTitle")}
+                    message={t("notificationPermissionsFriend")}
+                    time={t("notificationTime")}
+                    icon={Icon}
+                    onClose={handleCloseNotification}
+                />
+            )}
+        </div>
+    )
 }
-
-const UserProfile: React.FC<UserProfileProps> = () => {
-
-    const {session} = useSession();
-    let {webId} = session.info;
-
-
-    if (webId === undefined)
-        webId = "";
-
-    const dropdownTitle = (
-        <span>
-            <CombinedDataProvider datasetUrl={webId} thingUrl={webId}>
-                <Text property={FOAF.name.iri.value} autosave/>
-
-
-            </CombinedDataProvider>
-        </span>
-    );
-
-    return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            minHeight="100vh"
-            px={2}
-        >
-            <CombinedDataProvider datasetUrl={webId} thingUrl={webId}>
-                <Avatar sx={{width: 200, height: 200, mb: 2}}> <Image property={VCARD.hasPhoto.iri.value} width={200}/></Avatar>
-            </CombinedDataProvider>
-            <Typography variant="h4" gutterBottom textAlign="center">
-                {dropdownTitle}
-            </Typography>
-            <Typography variant="h6" gutterBottom textAlign="center">
-                @{"email"}
-            </Typography>
-            <CombinedDataProvider datasetUrl={webId} thingUrl={webId}>
-                <Typography variant="body2" color="textSecondary" component="p"
-                            style={{display: "flex", alignItems: "center"}}>
-                    @{"Mas info"}
-                </Typography>
-            </CombinedDataProvider>
-            <Box mt={4}>
-                <Button variant="contained">Editar perfil</Button>
-            </Box>
-
-        </Box>
-
-
-    );
-};
-
-export default UserProfile;
+export default ManageFriends
