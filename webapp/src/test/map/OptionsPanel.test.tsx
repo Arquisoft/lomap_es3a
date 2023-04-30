6050e89c864b70f3d40ad57df13efea23141e046
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-04-30 22:01:53 +0200

    30/04/2023 - Omar - Test sobre el panel de opciones

---
+++ webapp/src/test/map/OptionsPanel.test.tsx
@@ -0,0 +1,37 @@
+import {render} from "@testing-library/react";
+import OptionsPanel from "../../components/map/OptionsPanel";
+
+
+jest.mock('../../components/map/MapView', () => {
+    return function MockContactMap() {
+        return <div data-testid="mapMock">Mock</div>;
+    };
+});
+
+describe("OptionsPanel", () => {
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+
+    it("render menu title correctly", () => {
+        const {getByText} = render(<OptionsPanel setItem={() => {}} />);
+        expect(getByText("Map options")).toBeInTheDocument();
+    });
+
+    it("render MapSelector component", () => {
+        const {getByText} = render(<OptionsPanel setItem={() => {}} />);
+        expect(getByText("Map selector")).toBeInTheDocument();
+        expect(getByText("Create new map")).toBeInTheDocument();
+    });
+
+    it("render Filter component", () => {
+        const {getByText} = render(<OptionsPanel setItem={() => {}} />);
+        expect(getByText("Category")).toBeInTheDocument();
+    });
+
+    it("render Friends component", () => {
+        const {getByText} = render(<OptionsPanel setItem={() => {}} />);
+        expect(getByText("Friends")).toBeInTheDocument();
+    });
+});
