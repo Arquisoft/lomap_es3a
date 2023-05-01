c06059f918b0c8cf752c26804c1afc548ff35aa3
Raúl España <uo278036@uniovi.es>
2023-05-01 13:58:08 +0200

    01/05/2023 - Raúl - Añadidos nuevos componentes a pagina ABOUT

--- webapp/src/components/about/AboutPage.tsx
+++ webapp/src/components/about/AboutPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Box, Typography, Avatar } from "@mui/material";
+import {Box, Typography, Avatar, Card, CardMedia, CardContent, CardActions, Button} from "@mui/material";
 import { styled } from "@mui/material/styles";
 import GlobeComponent from "./Globe";
-
-
+import {PersonCard} from "./PersonCard";
+import "../../css/globe.css"
+import CarlosImg from "../../img/CarlosDiez.jpg";
+import OmarImg from "../../img/OmarImg.jpg";
 const DeveloperBox = styled(Box)({
     display: "flex",
     flexDirection: "column",
@@ -11,15 +13,46 @@ const DeveloperBox = styled(Box)({
     gap: "10px",
 });
 
+const Carlos = {
+    name: "Carlos Diez Fernández",
+    image: CarlosImg,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, libero eget mattis dignissim, quam orci ullamcorper nibh, id hendrerit elit eros ut lorem.",
+    github: "uo284373",
+};
+
+const Raul = {
+    name: "Raúl Fernández España",
+    image: OmarImg,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, libero eget mattis dignissim, quam orci ullamcorper nibh, id hendrerit elit eros ut lorem.",
+    github: "UO278036",
+};
+
+const Omar = {
+    name: "Omar Teixeira González",
+    image: OmarImg,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, libero eget mattis dignissim, quam orci ullamcorper nibh, id hendrerit elit eros ut lorem.",
+    github: "Omitg24",
+};
+
+const David = {
+    name: "David Leszek Warzynski Abril",
+    image: "/static/images/person.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, libero eget mattis dignissim, quam orci ullamcorper nibh, id hendrerit elit eros ut lorem.",
+    github: "UO278968",
+};
+
+
 const AboutPage = () => {
     return (
+
         <Box>
 
             <Typography variant="h2" align="center" mt={4}>
                 Sobre nosotros
             </Typography>
-
-            <GlobeComponent/>
+            <div id="globe">
+                <GlobeComponent/>
+            </div>
             <Box mt={6}>
                 <Typography variant="h4" align="center">
                     Desarrolladores
@@ -33,21 +66,21 @@ const AboutPage = () => {
                     mt={4}
                 >
                     <DeveloperBox>
-                        <Avatar src="https://i.pravatar.cc/150?img=1"/>
-                        <Typography variant="h5">Desarrollador 1</Typography>
-                        <Typography variant="subtitle1">Cargo 1</Typography>
+                        <PersonCard person={Carlos} />
+
                     </DeveloperBox>
 
                     <DeveloperBox>
-                        <Avatar src="https://i.pravatar.cc/150?img=2"/>
-                        <Typography variant="h5">Desarrollador 2</Typography>
-                        <Typography variant="subtitle1">Cargo 2</Typography>
-                    </DeveloperBox>
+                        <PersonCard person={Raul} />
 
+                    </DeveloperBox>
                     <DeveloperBox>
-                        <Avatar src="https://i.pravatar.cc/150?img=3"/>
-                        <Typography variant="h5">Desarrollador 3</Typography>
-                        <Typography variant="subtitle1">Cargo 3</Typography>
+                        <PersonCard person={Omar} />
+
+                    </DeveloperBox>
+                    <DeveloperBox>
+                        <PersonCard person={David} />
+
                     </DeveloperBox>
                 </Box>
             </Box>
@@ -88,7 +121,6 @@ const AboutPage = () => {
             </Box>
 
         </Box>
-
     );
 }
 export default AboutPage;
